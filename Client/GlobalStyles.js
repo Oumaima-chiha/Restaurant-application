cd6ebb4275347c7e409fba0ba5c3f9e8d8b6fdf4
Adam <abejaoui90@gmail.com>
2023-10-30 17:47:17 +0100

    Login screen is showing

---
+++ Client/GlobalStyles.js
@@ -0,0 +1,26 @@
+/* fonts */
+export const FontFamily = {
+    interRegular: "Inter-Regular",
+    interLight: "Inter-Light",
+    interBold: "Inter-Bold",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_base: 16,
+  };
+  /* Colors */
+  export const Color = {
+    colorBlack:'#231F20',
+    colorWhite: "#fff",
+    colorTomato: "#fb5757",
+  
+    colorRed: "#ff0000",
+  };
+  /* Paddings */
+  export const Padding = {
+    p_base: 16,
+  };
+  /* border radiuses */
+  export const Border = {
+    br_base: 16,
+  };
