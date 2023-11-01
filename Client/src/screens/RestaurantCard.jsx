5c7fa75897be76b9b5a09c408eac6efd71f69e59
Adam <abejaoui90@gmail.com>
2023-11-01 17:17:30 +0100

    restauant components added category added search bar fixed

---
+++ Client/src/screens/RestaurantCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Display } from "../utils";
+
+export default function RestaurantCard({ restaurant }) {
+  const { name, image, rating, status } = restaurant;
+
+  return (
+    <View style={styles.cardContainer}>
+      <Image source={image} style={styles.cardImage} />
+      <Text style={styles.cardName}>{name}</Text>
+      <Text style={styles.cardRating}>{`Rating: ${rating}`}</Text>
+      <Text style={styles.cardStatus}>{status}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+  },
+  cardImage: {
+    height:Display.setHeight(20),
+    width:Display.setWidth(90),
+  },
+  cardName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cardRating: {
+    color: 'gray',
+  },
+  cardStatus: {
+    color: 'green',
+  },
+});
