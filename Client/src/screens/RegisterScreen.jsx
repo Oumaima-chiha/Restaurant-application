73dc3c2f48f3e03269e5b7d649849813b24839d9
JawharZinelabidine <jawharzedd@gmail.com>
2023-11-03 23:07:57 +0100

    pulling

--- Client/src/screens/RegisterScreen.jsx
+++ Client/src/screens/RegisterScreen.jsx
@@ -1,6 +1,6 @@
 import { Colors } from '../contants';
 import React, { useState } from "react";
-import { Text, StyleSheet, View, TextInput, ScrollView, TouchableOpacity,   SafeAreaView } from 'react-native';
+import { Text, StyleSheet, View, TextInput, ScrollView, TouchableOpacity, SafeAreaView } from 'react-native';
 import Toast from 'react-native-toast-message';
 import axios from "axios";
 
@@ -43,7 +43,7 @@ const RegisterScreen = ({ navigation }) => {
   const handleSubmit = async () => {
     if (validator()) {
       try {
-        const { data } = await axios.post('http://192.168.137.37:3000/api/customers/', inputs);
+        const { data } = await axios.post('http://192.168.1.104:3000/api/customers/', inputs);
         console.log('User added successfully', data);
         Toast.show({
           type: 'success',
@@ -74,7 +74,7 @@ const RegisterScreen = ({ navigation }) => {
             Create a new account to access your portfolio and more
           </Text>
         </View>
-  
+
         <View style={styles.form}>
           <View style={styles.nameInputs}>
             <View s style={[styles.input, styles.largeInput]}>
@@ -82,24 +82,24 @@ const RegisterScreen = ({ navigation }) => {
               <TextInput
                 autoCapitalize="words"
                 placeholder="John"
-              onChangeText={(text) => handleChange('firstName', text)}  
+                onChangeText={(text) => handleChange('firstName', text)}
                 placeholderTextColor="#6b7280"
                 style={styles.inputControl}
               />
             </View>
-  
+
             <View s style={[styles.input, styles.largeInput]}>
               <Text style={styles.inputLabel}>Last Name</Text>
               <TextInput
                 autoCapitalize="words"
                 placeholder="Doe"
-                onChangeText={(text) => handleChange('lastName', text)} 
+                onChangeText={(text) => handleChange('lastName', text)}
                 placeholderTextColor="#6b7280"
                 style={styles.inputControl}
               />
             </View>
           </View>
-  
+
           <View style={styles.input}>
             <Text style={styles.inputLabel}>Email address</Text>
             <TextInput
@@ -112,7 +112,7 @@ const RegisterScreen = ({ navigation }) => {
               style={styles.inputControl}
             />
           </View>
-  
+
           <View style={styles.input}>
             <Text style={styles.inputLabel}>Password</Text>
             <TextInput
@@ -124,17 +124,17 @@ const RegisterScreen = ({ navigation }) => {
               secureTextEntry={true}
             />
           </View>
-  
+
           <View style={styles.formAction}>
-            <TouchableOpacity  onPress={handleSubmit}>
+            <TouchableOpacity onPress={handleSubmit}>
               <View style={styles.btn}>
                 <Text style={styles.btnText}>Sign up</Text>
               </View>
-              
+
             </TouchableOpacity>
           </View>
-  
-          <TouchableOpacity style={{ marginTop: 'auto' }}   onPress={handleButtonPress}>
+
+          <TouchableOpacity style={{ marginTop: 'auto' }} onPress={handleButtonPress}>
             <Text style={styles.formFooter}>
               Already have an account?{' '}
               <Text style={{ textDecorationLine: 'underline', color: Colors.DEFAULT_RED }}>
@@ -146,95 +146,95 @@ const RegisterScreen = ({ navigation }) => {
       </View>
     </SafeAreaView>
   );
-  }
-  const styles = StyleSheet.create({
-    container: {
-      padding: 24,
-      flexGrow: 1,
-      flexShrink: 1,
-      flexBasis: 0,
-    },
-    nameInputs: {
-      flexDirection: 'row', // Place first name and last name inputs side by side
-      justifyContent: 'space-between', // Add some space between the inputs
-    },
-    largeInput: {
-      width: 150, // Adjust the height as needed
-    },
-    header: {
-      marginVertical: 36,
-    },
-    headerImg: {
-      width: 80,
-      height: 80,
-      alignSelf: 'center',
-      marginBottom: 36,
-    },
-    title: {
-      fontSize: 27,
-      fontWeight: '700',
-      color: Colors.DEFAULT_WHITE,
-      marginBottom: 6,
-      textAlign: 'center',
-    },
-    subtitle: {
-      fontSize: 15,
-      fontWeight: '500',
-      color: '#929292',
-      textAlign: 'center',
-    },
-    form: {
-      marginBottom: 130,
-      flexGrow: 1,
-      flexShrink: 1,
-      flexBasis: 0,
-    },
-    formAction: {
-      marginVertical: 24,
-    },
-    formFooter: {
-      fontSize: 17,
-      fontWeight: '600',
-      color: Colors.DEFAULT_WHITE,
-      textAlign: 'center',
-      letterSpacing: 0.15,
-    },
-    input: {
-      marginBottom: 16,
-    },
-    inputLabel: {
-      fontSize: 17,
-      fontWeight: '600',
-      color: Colors.DEFAULT_WHITE,
-      marginBottom: 8,
-    },
-    inputControl: {
-      height: 44,
-      backgroundColor:Colors.DEFAULT_BLACK ,
-      borderColor: Colors.DEFAULT_RED,
-      borderWidth: 1, 
-      paddingHorizontal: 16,
-      borderRadius: 12,
-      fontSize: 15,
-      fontWeight: '500',
-      color: Colors.DEFAULT_WHITE,
-    },
-    btn: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: 8,
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderWidth: 1,
-      backgroundColor: Colors.DEFAULT_RED,
-      borderColor: Colors.DEFAULT_RED,
-    },
-    btnText: {
-      fontSize: 18,
-      lineHeight: 26,
-      fontWeight: '600',
-      color: '#fff',
-    },
-  });
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  nameInputs: {
+    flexDirection: 'row', // Place first name and last name inputs side by side
+    justifyContent: 'space-between', // Add some space between the inputs
+  },
+  largeInput: {
+    width: 150, // Adjust the height as needed
+  },
+  header: {
+    marginVertical: 36,
+  },
+  headerImg: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    marginBottom: 36,
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: '700',
+    color: Colors.DEFAULT_WHITE,
+    marginBottom: 6,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#929292',
+    textAlign: 'center',
+  },
+  form: {
+    marginBottom: 130,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  formAction: {
+    marginVertical: 24,
+  },
+  formFooter: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: Colors.DEFAULT_WHITE,
+    textAlign: 'center',
+    letterSpacing: 0.15,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: Colors.DEFAULT_WHITE,
+    marginBottom: 8,
+  },
+  inputControl: {
+    height: 44,
+    backgroundColor: Colors.DEFAULT_BLACK,
+    borderColor: Colors.DEFAULT_RED,
+    borderWidth: 1,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: '500',
+    color: Colors.DEFAULT_WHITE,
+  },
+  btn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    backgroundColor: Colors.DEFAULT_RED,
+    borderColor: Colors.DEFAULT_RED,
+  },
+  btnText: {
+    fontSize: 18,
+    lineHeight: 26,
+    fontWeight: '600',
+    color: '#fff',
+  },
+});
 export default RegisterScreen;
